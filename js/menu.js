432b06d766c3588a742b6ba1f75aad51644d9b20
EstefanoA21 <42593956+EstefanoA21@users.noreply.github.com>
2019-09-03 00:18:11 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,22 @@
 (function () {
   "use strict";
-
   $(function () {
     $(".menu-container").load("menu.html");
   });
 })();
+
+window.onscroll = function() {myFunction()};
+
+var menu = document.getElementById("menu");
+
+function myFunction() {
+    
+  var menu = document.getElementById("menu");
+  var y = document.scrollTop;
+    
+  if (window.pageYOffset > 350) {
+    menu.classList.add("sticky");
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
